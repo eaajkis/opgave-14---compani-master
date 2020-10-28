748457044634bf40403e7ecf90613f824c6e1f4c
Jonas Kisum <eaajkis@students.eaaa.dk>
2020-10-28 11:37:41 +0100

    master

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const config = {
-    databaseURI: 'mongodb+srv://imnotanowl:lowbob123@fredescluster.b0brk.mongodb.net/chatArkitektur?retryWrites=true&w=majority', // or MongoDB Atlas connection URI
+    databaseURI: 'mongodb+srv://kisum21:211997Jk@cluster0.6wlpe.mongodb.net/companyDB?retryWrites=true&w=majority', // or MongoDB Atlas connection URI
     localPort: 8090
 };
 
